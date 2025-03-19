d9c9713b81bbd5ef95021c8562a1ec932d7080da
Anna Letícia Silva <annaleticiabarbosadasilva@gmail.com>
2025-03-18 21:35:28 -0300

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+    int main (void) {
+        float nota1;
+        float nota2;
+        float nota3;
+    
+        printf("Digite a primeira nota:");
+        scanf("%f", &nota1);
+    
+        printf("Digite a segunda nota:");
+        scanf("%f", &nota2);
+    
+        printf("Digite a terceira nota:");
+        scanf("%f", &nota3);
+    
+        float media = (nota1 + nota2 + nota3) /3;
+    
+        printf("Média das notas: %.2f", media);
+    
+    return 0;
+    }
