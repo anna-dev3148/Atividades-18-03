81e0716df142a61dc43280e4c2a37522e855673e
Anna Letícia Silva <annaleticiabarbosadasilva@gmail.com>
2025-03-18 21:37:33 -0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -4,13 +4,13 @@
         float nota2;
         float nota3;
     
-        printf("Digite a primeira nota:");
+        printf("Digite a primeira nota: ");
         scanf("%f", &nota1);
     
-        printf("Digite a segunda nota:");
+        printf("Digite a segunda nota: ");
         scanf("%f", &nota2);
     
-        printf("Digite a terceira nota:");
+        printf("Digite a terceira nota: ");
         scanf("%f", &nota3);
     
         float media = (nota1 + nota2 + nota3) /3;
